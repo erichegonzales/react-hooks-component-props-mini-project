c31c20f223cd72d7849c63c75787fae8f1c31299
Eriche Gonzales <erichelouise@gmail.com>
2022-07-21 14:09:38 -0400

    Done with lab

---
+++ src/components/Article.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const minutesEmoji = (minutes) => {
+    let emoji = '☕️'
+    let number = Math.floor(minutes / 5)
+
+    if (minutes >= 30) {
+        emoji = '🍱'
+        number = Math.floor(minutes / 10) 
+    }
+
+    return emoji.repeat(number)
+}
+
+const Article = ({ title, date ='January 1, 1970', preview, minutes}) => {
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date} • {minutesEmoji(minutes)} {minutes} min read</small>
+            <p>{preview}</p>
+        </article>
+    )
+}
+
+export default Article
+
